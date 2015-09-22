9219f5db6399e151355dc029203cb47d7a8256a3
Slamkas <seslamka@yahoo.com>
2015-09-22 11:26:16 -0700

    Update proj_1.c

--- Project1/proj_1.c
+++ Project1/proj_1.c
@@ -3,10 +3,11 @@
 int main()
 {
 	puts("hi");
-	struct node** head = NULL;
-	struct node*  item = NULL;
+	struct node** head;
+	struct node*  item;
 	initQueue(head);
 	item = newItem();
 	addQueue(head, item);
+	delQueue(head);
 	return 0;
 }
